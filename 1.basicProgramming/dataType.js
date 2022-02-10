2c0a3db7e6105776f1202d4644ab276e8974921f
Tanjila Akter <tanjila.akter@bjitgroup.com>
2022-02-10 11:35:27 +0600

    javascript start

---
+++ 1.basicProgramming/dataType.js
@@ -0,0 +1,65 @@
+/* Data Type: 
+1.Primitive Data Type ==> Javascript has only 5 data types which are: 
+1.1.String
+1.2.Number
+1.3.Boolean
+1.4.Null
+1.5.Undefined
+2.Object Data Type ==> Javascript has only 2 data types which are:
+2.1.Object
+2.2.Array
+3.Reference Data Type ==> Javascript has only 3 data types which are:
+3.1.Function
+3.2.Date
+3.3.RegExp
+4.Symbol Data Type ==> Javascript has only 1 data type which is:
+4.1.Symbol
+5.Error Data Type ==> Javascript has only 1 data type which is:
+5.1.Error
+6.Promise Data Type ==> Javascript has only 1 data type which is:
+6.1.Promise
+7.Map Data Type ==> Javascript has only 1 data type which is:
+7.1.Map
+8.Set Data Type ==> Javascript has only 1 data type which is:
+8.1.Set
+9.WeakMap Data Type ==> Javascript has only 1 data type which is:
+9.1.WeakMap
+10.WeakSet Data Type ==> Javascript has only 1 data type which is:
+10.1.WeakSet
+11.Proxy Data Type ==> Javascript has only 1 data type which is:
+11.1.Proxy
+12.Int8Array Data Type ==> Javascript has only 1 data type which is:
+12.1.Int8Array
+13.Uint8Array Data Type ==> Javascript has only 1 data type which is:
+13.1.Uint8Array
+14.Uint8ClampedArray Data Type ==> Javascript has only 1 data type which is:
+14.1.Uint8ClampedArray
+15.Int16Array Data Type ==> Javascript has only 1 data type which is:
+15.1.Int16Array
+16.Uint16Array Data Type ==> Javascript has only 1 data type which is:
+16.1.Uint16Array
+17.Int32Array Data Type ==> Javascript has only 1 data type which is:
+17.1.Int32Array
+18.Uint32Array Data Type ==> Javascript has only 1 data type which is:
+18.1.Uint32Array
+19.Float32Array Data Type ==> Javascript has only 1 data type which is:
+19.1.Float32Array
+20.Float64Array Data Type ==> Javascript has only 1 data type which is:
+20.1.Float64Array
+21.BigInt64Array Data Type ==> Javascript has only 1 data type which is:
+21.1.BigInt64Array
+22.BigUint64Array Data Type ==> Javascript has only 1 data type which is:
+22.1.BigUint64Array
+23.ArrayBuffer Data Type ==> Javascript has only 1 data type which is:
+23.1.ArrayBuffer
+24.SharedArrayBuffer Data Type ==> Javascript has only 1 data type which is:
+24.1.SharedArrayBuffer
+25.Atomics Data Type ==> Javascript has only 1 data type which is:
+25.1.Atomics
+26.DataView Data Type ==> Javascript has only 1 data type which is:
+26.1.DataView
+27.JSON Data Type ==> Javascript has only 1 data type which is:
+27.1.JSON
+28.Promise Data Type ==> Javascript has only 1 data type which is:
+28.1.Promise
+2.Object Data Type */
---
+++ 1.basicProgramming/variable.js
@@ -0,0 +1,12 @@
+let name = "Tanjila Shamima" // string data type
+let age = 24 // number data type
+let isMarried = false // boolean data type
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+if(isMarried){
+    console.log("Married");
+}
+else{
+    console.log("Not Married");
+}
